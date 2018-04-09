ed3c09db5c05f7d156f3d1f25b9eca2945fda0dd
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-04-09 17:50:50 +0700

    Test App js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import Navbar from './components/Navbar'
+import { Switch } from 'react-router-dom'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('<App />', () => {
+  it('should render Navbar', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.contains(<Navbar />)).toBe(true)
+  })
+
+})
