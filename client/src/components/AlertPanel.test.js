93706a6a795ce26956d6ea22e62b1a0c4787546a
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-04-09 17:50:56 +0700

    Test AlertPanel

---
+++ client/src/components/AlertPanel.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import AlertPanel from './AlertPanel'
+import { Alert } from 'react-bootstrap'
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('<AlertPanel />', () => {
+  it('should render Alert span because has no props ', () => {
+    const wrapper = shallow(<AlertPanel />)
+    expect(wrapper.contains(<span></span>)).toBe(true)
+  })
+
+  const wrapper = shallow(<AlertPanel />)
+  it('should render Alert with message terjadi kesalahan', () => {
+    const wrapper = render(<AlertPanel show={true} status="danger" message="terjadi kesalahan" />)
+    expect(wrapper.text()).toBe("terjadi kesalahan")
+  })
+
+})
